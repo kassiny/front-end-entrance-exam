37d73cf6162ee14329106f3393b628c4065f35ba
Maria <razumovahome@mail.ru>
2024-08-05 09:11:02 +0300

    saves some text

--- js/source.js
+++ js/source.js
@@ -1,11 +1,8 @@
+window.jsPDF = window.jspdf.jsPDF;
 
 function savePDF() {
   //  document.getElementById('download-pdf').addEventListener('click', function() {
-        let images = document.getElementsByTagName("img");
-        for (let i = 0; i < images.length; i ++)
-        {
-            images[i].crossOrigin = "anonymous";
-        }
+
         const element = document.getElementById('mainbody'); // Get the HTML element to be converted to PDF
         html2canvas(element).then(canvas => {
 
@@ -20,12 +17,19 @@ function savePDF() {
     //    });
 }
 
-function hellooooooo() {
-    alert("hell");
-}
 
 function getEditableText() {
     getExperienceTimeline("timelinerecent", "<p> jun 2023 - Present</p>");
+    getExperienceTimeline("languageNamesList", "<p>English</p><p>Malayalam</p><p>Hindi</p>");
+    getExperienceTimeline("feauturedPoints"," <p><ul><li>Strategy development and planning of campaigns that promote the business and generate genuine traffic</li>" + 
+    "<li>SEO Content Creation for Blogs, Website, Social media</li></ul></p>");
+    getExperienceTimeline("timeline2","<p> 2017 - Present</p>");
+    getExperienceTimeline("rolephrase", "<p>UX/UI Designer</p>");
+    getExperienceTimeline("job1","<div><p>Marketing Manager</p></div><div><p>Pankayam|full-time</p></div>");
+    getExperienceTimeline("job2","<div><p>Marketing Manager</p></div><div><p>Pankayam|full-time</p></div>");
+    getExperienceTimeline("feauturedPoints2"," <p><ul><li>Strategy development and planning of campaigns that promote the business and generate genuine traffic</li>" + 
+    "<li>SEO Content Creation for Blogs, Website, Social media</li></ul></p>");
+    getExperienceTimeline("timeline3","<p> sep 2021 - Jun 2023</p>");
 }
 
 function getExperienceTimeline(id, defaultText ) {
@@ -41,7 +45,7 @@ function setEditableText(id) {
     if (sessionStorage)
     {
         console.log(id);
-        sessionStorage.setItem("ss_"+id, document.getElementById(id).innerHTML );
+        sessionStorage.setItem("ss_" + id, document.getElementById(id).innerHTML );
     }
     console.log(document.getElementById(id).innerText);
 }
